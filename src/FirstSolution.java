b4cb3965a67ed55e780c41bba91f10e5d2c025f7
Abraao-Morais <duarte.abraao30@gmail.com>
2024-11-16 22:01:10 -0300

    ......

--- src/FirstSolution.java
+++ src/FirstSolution.java
@@ -4,67 +4,82 @@ import java.util.Objects;
 
 public class FirstSolution {
 
-    public static void firstSolution(Plate[] championship) {
-        int generalSum = 0;
-        for (Plate plate : championship)
-            generalSum += plate.sumAB();
+    private static final int MULTIPLIER = 150;
 
+    public static void firstSolution(Plate[] championship) {
+        boolean hasDiscartedPlate = getGeneralSum(championship) % 2 != 0;
         int solution = Integer.MIN_VALUE;
         String solutionString = null;
-        boolean hasDiscartedPlate = generalSum % 2 != 0;
         Plate discartedPlate = null;
+        List<Integer> discardedPlatesTested = new ArrayList<>();
 
-        for (int i = 1; i <= 100 * championship.length; i++) {
-            int numberRandomPlates = (int) (Math.random() * championship.length - 1) + 1;
-
-            List<Integer> randomPlates = new ArrayList<>();
-            for (int j = 1; j <= numberRandomPlates; j++)
-                randomPlates.add((int) (Math.random() * championship.length));
-
-            for (int j = 1; j <= championship.length; j++) {
-                if (randomPlates.contains(j)) {
-                    int a = championship[j].getA();
-                    int b = championship[j].getB();
-                    championship[j].setA(b);
-                    championship[j].setB(a);
-                }
-            }
+        for (int i = 1; i <= MULTIPLIER * championship.length; i++) {
+            int numberRandomPlates = (int) (Math.random() * championship.length -1) +1;
+            List<Integer> randomPlates = getRandomPlates(championship, numberRandomPlates);
+            invertPlates(championship, randomPlates);
 
             int top = 0;
             int down = 0;
-
             if (hasDiscartedPlate) {
-                int indexDiscardedPlate = (int) (Math.random() * championship.length);
 
-                for (int j = 1; j <= championship.length; j++) {
+                int indexDiscardedPlate = (int) (Math.random() * championship.length);
+                while (discardedPlatesTested.contains(indexDiscardedPlate) && indexDiscardedPlate != championship.length)
+                    indexDiscardedPlate++;
+
+                for (int j = 0; j < championship.length; j++) {
                     if (j == indexDiscardedPlate)
                         continue;
-                    top += championship[j - 1].getA();
-                    down += championship[j - 1].getB();
+                    top += championship[j].getA();
+                    down += championship[j].getB();
                 }
-                if (top == down) {
-                    if (Objects.isNull(discartedPlate) || discartedPlate.sumAB() < championship[indexDiscardedPlate].sumAB())
+                if (top == down && top >= solution) {
+                    if (Objects.isNull(discartedPlate)
+                            || (championship[indexDiscardedPlate].getA() <= championship[indexDiscardedPlate].getB()
+                                    && championship[indexDiscardedPlate].getA() < discartedPlate.getA())) {
                         discartedPlate = championship[indexDiscardedPlate];
-                    solution = top;
+                        discardedPlatesTested.add(indexDiscardedPlate);
+                        solution = top;
+                        solutionString = solution + " descartada a placa " + discartedPlate.toString();
+                    }
                 }
             } else {
-
                 for (int j = 1; j <= championship.length; j++) {
                     top += championship[j - 1].getA();
                     down += championship[j - 1].getB();
                 }
-                if (top == down && top > solution)
+                if (top == down && top > solution) {
                     solution = top;
+                    solutionString = solution + " nenhuma placa descartada";
+                }
             }
-
-            if (Objects.nonNull(discartedPlate))
-                solutionString = solution + " descartada a placa " + discartedPlate.toString();
-            if (Objects.isNull(solutionString) || solutionString.equals("impossivel"))
-                solutionString = "impossivel";
-            else
-                solutionString = solution + " nenhuma placa descartada";
-
         }
+        if (Objects.isNull(solutionString))
+            solutionString = "impossivel";
         System.out.println(solutionString);
+    }
+
+    private static int getGeneralSum(Plate[] championship){
+        int generalSum = 0;
+        for (Plate plate : championship)
+            generalSum += plate.sumAB();
+        return generalSum;
+    }
+
+    private static List<Integer> getRandomPlates(Plate[] championship, int numberRandomPlates){
+        List<Integer> randomPlates = new ArrayList<>();
+        for (int j = 1; j <= numberRandomPlates; j++)
+            randomPlates.add((int) (Math.random() * championship.length));
+        return randomPlates;
+    }
+
+    private static void invertPlates(Plate[] championship, List<Integer> randomPlates){
+        for (int j = 0; j < championship.length; j++) {
+            if (randomPlates.contains(j)) {
+                int a = championship[j].getA();
+                int b = championship[j].getB();
+                championship[j].setA(b);
+                championship[j].setB(a);
+            }
+        }
     }
 }
