1df1c668029f07f956b5edef82e191d4e1de48bc
Giovani <giovanifetzner01@gmail.com>
2024-11-16 22:04:58 -0300

    Adição secondSolution

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+        secondSolution();
+    }
+
+    private static void firstSolution() {
         int count = 0;
         do {
             try {
@@ -15,7 +19,24 @@ public class Main {
                 }
                 count++;
             } catch (FileNotFoundException e){
-                System.out.println("Caiu no catch");
+                System.out.println("Erro no arquivo");
+                break;
+            }
+        }while (true);
+    }
+
+    private static void secondSolution() {
+        int count = 0;
+        do {
+            try {
+                System.out.println("\n-------------------------\n");
+                List<Plate[]> championships = ExtractData.extractData(count);
+                for(int i = 0; i < championships.size(); i++){
+                    SecondSolution.secondSolution(championships.get(i));
+                }
+                count++;
+            } catch (FileNotFoundException e){
+                System.out.println("Erro no arquivo");
                 break;
             }
         }while (true);
---
+++ src/SecondSolution.java
@@ -0,0 +1,5 @@
+public class SecondSolution {
+    public static String secondSolution(Plate[] championship){
+        return null;
+    }
+}
